ae246fc860f31e459931eef5d26b9e8a548ab024
trinadhdn <trinadhdn@gmail.com>
2024-04-22 09:25:20 +0530

    sets and typeOf

---
+++ jslearn/src/sets.js
@@ -0,0 +1,47 @@
+
+let arr = ['a', 'b', 'a']
+
+console.log(arr)
+
+let setOfElements = new Set(['a', 'b', 'a'])
+
+console.log(setOfElements)
+
+let setList = new Set();
+
+let val = 'first'
+setList.add(val)
+setList.add('second')
+setList.add('thrid')
+
+console.log(setList)
+
+// for (let x of setList.values()) {
+//     console.log(x)
+// }
+
+setList.size
+
+//eg :- testdata within csv
+// name,age,desg
+//  kumar,40,qe
+//  suresh.41,qe
+
+
+let csvTestdata = "filename"
+
+csvTestdata.forEach(function (data) {
+    // 100 line test cases
+    
+    data.name
+
+
+})
+setList.forEach(function(value) {
+console.log(value)
+test()
+})
+
+//set --> add(), delete(), foreach, values(), has('first'), size
+
+
---
+++ jslearn/src/typeOfAndTypeConvesion.js
@@ -0,0 +1,17 @@
+//jS types -->
+// string
+// boolean
+// number
+// object ==> array, set , string ,
+// function
+
+// null, undefined
+
+
+let x = function () { }
+console.log(typeof(x))
+
+
+
+// task 
+// Practice sets , foreach , typeof , write funtions and assign to variables
