3de31e76dff8c84aacf308d8acd2cd16e36a2ef4
trinadhdn <trinadhdn@gmail.com>
2024-04-25 09:16:02 +0530

    i removed the spaces

--- jslearn/src/class2.js
+++ jslearn/src/class2.js
@@ -3,9 +3,7 @@ const Car = require('./class')
 class usingCarClass {
     constructor() {
         let carObject = new Car('Astor')
-
         console.log(carObject.carModle('MG'))
-
         console.log(carObject.carVersion('Astor'))
     }
 }
